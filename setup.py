285fd65ba9de3cc5a2a1ae5a47be98ccf15a16fc
nishanthreddy <nishanthreddy495@gmail.com>
2024-05-02 13:03:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_DOT='-e .'
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_DOT in requirements:
+            requirements.remove(HYPEN_DOT)
+    return requirements
+
+setup(
+    name='Student_Performance_Prediction_ML',
+    version='0.0.1',
+    author='nishanth2002',
+    author_email='nishanthreddy495@gmail.com',
+    packages=find_packages(),
+    install_reuires=get_requirments('requirements.txt')
+
+)
